105179c9868524736c0b7a05adacbee8abe4da44
Anupam Saikia <30382168+anupamsaikia@users.noreply.github.com>
2018-08-18 17:42:39 +0530

    added home page

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,133 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
+  <v-app id="inspire">
+<!--     <my-loader v-show="loading" style="z-index:100"></my-loader>
+ -->    <v-navigation-drawer
+      :clipped="$vuetify.breakpoint.lgAndUp"
       v-model="drawer"
-      enable-resize-watcher
       fixed
       app
     >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
+      <v-list dense>
+        <template v-for="item in items">
+          <v-layout
+            v-if="item.heading"
+            :key="item.heading"
+            row
+            align-center
+          >
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-flex>
+            <v-flex xs6 class="text-xs-center">
+              <a href="#!" class="body-2 black--text">EDIT</a>
+            </v-flex>
+          </v-layout>
+
+          <v-list-tile v-else :key="item.text" :to="'/'+item.target">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+
+        </template>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      color="white"
       app
-      :clipped-left="clipped"
+      fixed
+      :dense="$vuetify.breakpoint.mdAndDown"
     >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-toolbar-title style="width: 300px" class="">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <span class="">{{ title }}</span>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
+      <v-menu
+        left
+        nudge-left="40"
+      >
+        <v-btn
+          slot="activator"
+          icon
+        >
+          <v-icon>{{ ($store.state.authToken)?"person":"more_vert" }}</v-icon>
+        </v-btn>
+
+        <v-list>
+          <v-list-tile v-if="!$store.state.authToken" @click="()=>{}">
+            <v-list-tile-title>Admin Login</v-list-tile-title>
+          </v-list-tile>
+          <v-list-tile v-else @click="logout()">
+            <v-list-tile-title>Logout</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
     </v-toolbar>
+
+
+    <!-- <v-dialog max-width="600" :value="$store.state.adminLoginDialog">
+      <admin-login :next="$store.state.loginNext" @close="toggleLoginForm"></admin-login>
+    </v-dialog> -->
+
+
     <v-content>
       <router-view/>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
+  //import Loader from '@/components/loader'
+  //import AdminLogin from '@/components/adminLogin'
+  //import { mapActions } from 'vuex'
 
-export default {
-  name: 'App',
-  data () {
-    return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
+  export default {
+    /* components: {
+      'my-loader': Loader,
+      AdminLogin,
+    }, */
+    
+    data: () => ({
+      title: 'Robotics Club',
+      drawer: null,
+      items: [
+        { icon: 'home', text: 'Home', target: '' },
+        { icon: 'person_add', text: 'Join Club', target: 'join' },
+        { icon: 'event', text: 'Events', target: 'events' },
+        { icon: 'android', text: 'Projects', target: 'projects' },
+        { icon: 'group', text: 'Members', target: 'members' },
+        { icon: 'book', text: 'Tutorials', target: 'tutorials' },
+        { icon: 'photo_library', text: 'Gallery', target: 'gallery' },
+      ],
+
+      //adminLoginDialog: false,
+
+    }),
+    
+    name: 'App'
   }
-}
 </script>
+
+<style>
+a{
+  text-decoration: none;
+  color: inherit;
+  font:inherit;
+}
+
+.my-link{
+  width:100%; 
+}
+
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,124 @@
 <template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-layout>
-    </v-slide-y-transition>
+<div>
+<v-jumbotron v-if="$vuetify.breakpoint.mdAndUp" class="anupam">
+  <v-container fill-height>
+    <v-layout align-center>
+      <v-flex>
+        <h3 class="headline" style="color:#ffa100">Welcome to</h3>
+        <h3 class="display-1 blue--text">Robotics Club</h3>
+        <h3 class="headline grey--text text--darken-2">Central Institute of Technology Kokrajhar</h3>
+
+        <span class="subheading"></span>
+
+        <div class="body-2 mt-5">If you are enthusiastic about robotics, fill the form to join the club</div>
+        <v-btn
+          class="mx-0"
+          color="primary"
+          large
+          to="/join"
+        >
+          Join Club
+        </v-btn>
+      </v-flex>
+    </v-layout>
   </v-container>
+</v-jumbotron>
+
+<v-jumbotron v-if="$vuetify.breakpoint.smAndDown" class="anupam2">
+  <v-container fill-height>
+    <v-layout align-center>
+      <v-flex>
+        <h3 class=" headline" style="color:#ffa100">Welcome to</h3>
+        <h3 class=" display-1 blue--text">Robotics Club</h3>
+        <br>
+        <span class=" at headline grey--text text--darken-3">Central Institute of Technology Kokrajhar</span>
+
+        <span class="subheading"></span>
+
+        <div class=" at white body-2 mt-5">If you are enthusiastic about robotics, fill the form to join the club</div>
+        <v-btn
+          class="mx-0"
+          color="primary"
+          large
+          to="/join"
+        >
+          Join Club
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</v-jumbotron>
+
+
+<div>
+  <v-layout row class="my-3 white">
+    <v-flex xs6 sm6 class="pa-3">
+      <a href="https://youtube.com" style="display:flex; justify-content:center;">
+        <v-avatar>
+          <img src="../assets/youtube.png" alt="Youtube">
+        </v-avatar>
+        <span class="pa-3 subheading">Youtube</span>
+      </a>
+    </v-flex>
+
+    <v-flex xs6 sm6 class="pa-3">
+      <a href="https://facebook.com" style="display:flex; justify-content:center;">
+        <v-avatar>
+          <img src="../assets/facebook.png" alt="Facebook">
+        </v-avatar>
+        <span class="pa-3 subheading">Facebook</span>
+      </a>
+    </v-flex>
+
+  </v-layout>
+</div>
+
+
+</div>
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<script>
+export default {
+
+  computed: {
+    imageHeight () {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs': return '500px'
+        case 'sm': return '400px'
+        case 'md': return '500px'
+        case 'lg': return '500px'
+        case 'xl': return '800px'
+      }
+    },
+
+  
+  },
+
+  
+}
+</script>
+
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .anupam{
+    background-color: white;
+    background-image: url('../assets/bg.png');
+    background-size: contain;
+    background-position-x: right;
+    background-position-y: top;
+  }
+
+  .anupam2{
+    background-color: white;
+    background-image: url('../assets/bg.png');
+    background-size: 250%;
+    background-position-y: top;
+    background-position-x: left;
+  
+  }
+
+  .at {
+   background-color: white!important;
+   opacity: 0.65;
+ }
+
 </style>
