b2670db4c408814f3a7cd14f20ce613b5cf92ecf
Anupam Saikia <30382168+anupamsaikia@users.noreply.github.com>
2018-08-19 03:20:10 +0530

    added store state currentUser

--- src/store.js
+++ src/store.js
@@ -5,10 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    currentUser : null
   },
   mutations: {
-
+    setCurrentUser(state, val) {
+      state.currentUser = val
+    },
   },
   actions: {
 
