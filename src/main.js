32050ca9cf8209a7f70dde454c6fc13fc64290a4
Anupam Saikia <30382168+anupamsaikia@users.noreply.github.com>
2018-08-19 03:19:32 +0530

    added firebase init code in main.js

--- src/main.js
+++ src/main.js
@@ -5,19 +5,27 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
-const fb = require('./firebase/index')
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/storage'
+
+
+var config = {
+  apiKey: "AIzaSyATFMI2Mr1LFsCLQ-OUIWpakZ7xf2oRS2Q",
+  authDomain: "robotics-club-cit.firebaseapp.com",
+  databaseURL: "https://robotics-club-cit.firebaseio.com",
+  projectId: "robotics-club-cit",
+  storageBucket: "robotics-club-cit.appspot.com",
+};
+firebase.initializeApp(config);
+
 
 Vue.config.productionTip = false
 
-// handle page reloads
-let app
-fb.auth.onAuthStateChanged(user => {
-  if (!app) {
-    app = new Vue({
-      el: '#app',
-      router,
-      store,
-      render: h => h(App)
-    })
-  }
-})
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
