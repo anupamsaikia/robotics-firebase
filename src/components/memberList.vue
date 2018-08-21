4e442a3b49a6a777ddcc82da0db881e19804145b
Anupam Saikia <30382168+anupamsaikia@users.noreply.github.com>
2018-08-22 00:47:52 +0530

    added member view

---
+++ src/components/memberList.vue
@@ -0,0 +1,124 @@
+<template>
+<div>
+  <v-list two-line>
+    <template v-for="(member, index) in members">
+      <v-list-tile
+        :key="member.id"
+        avatar
+        @click="() => {}"
+      >
+        <v-list-tile-avatar>
+          <img :src="member.avatar">
+        </v-list-tile-avatar>
+        
+          <v-list-tile-content>
+            <router-link :to="'/members/' + member.id" class="my-link">
+              <v-list-tile-title class="body-2 grey--text text--darken-3" v-text="member.name"></v-list-tile-title>
+              <v-list-tile-sub-title class="body-1" v-text="member.role"></v-list-tile-sub-title>
+              </router-link>
+          </v-list-tile-content>
+        
+      </v-list-tile>
+
+      <v-divider
+        v-if="index + 1 < members.length"
+        :inset="true"
+        :key="index"
+      ></v-divider>
+    </template>
+  </v-list>
+
+  <div v-if="message" class="my-5">
+    <p class="title py-5 px-2 grey--text text--darken-2" style="text-align:center">{{ message }}</p>
+  </div>
+  
+
+</div>
+</template>
+
+
+<script>
+import firebase from 'firebase/app'
+const db = firebase.firestore()
+const settings = {timestampsInSnapshots: true};
+db.settings(settings);
+
+export default {
+
+  data(){
+    return {
+      members: [],
+
+      //to show msg in case of no member present
+      message: null,
+
+    }
+  },
+
+  mounted(){
+    this.getData()
+  },
+
+  //core_member, faculty, member
+  props:['mtype'],
+  
+  methods:{
+
+    /* ...mapActions([
+      'loading'
+    ]), */
+
+    getData(){
+      //this.loading('start')
+
+      if(this.mtype == "core_member"){
+        db.collection("members")
+        .where("visibility", "==", "public")
+        .where("level",">=",5)
+        .orderBy("level", "desc")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            this.members.push({...doc.data(), ...{'id' : doc.id} })
+          });
+          if(this.members.length == 0){
+            this.message = "No member available"
+          }
+        })
+        .catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
+      } else {
+        var level = (this.mtype == 'faculty')?4:3;
+        db.collection("members")
+        .where("visibility", "==", "public")
+        .where("level","==",level)
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            this.members.push({...doc.data(), ...{'id' : doc.id} })
+          });
+          if(this.members.length == 0){
+            this.message = "No member available"
+          }
+        })
+        .catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
+      }
+
+
+      
+    },
+
+
+
+
+  },
+
+
+
+
+}
+</script>
+
---
+++ src/views/Members.vue
@@ -0,0 +1,57 @@
+<template>
+  <!-- Members tabs -->
+  <v-layout row class="my-3">
+    <v-flex xs12 sm8 offset-sm2>
+      <v-card>
+        <v-tabs
+          v-model="tab"
+          color="grey lighten-3"
+          grow
+        >
+          <v-tabs-slider color="blue"></v-tabs-slider>
+
+          <v-tab :ripple="false" key="core_members">Core Members</v-tab>
+          <v-tab :ripple="false" key="faculty">Faculty</v-tab>
+          <v-tab :ripple="false" key="members">Members</v-tab>
+
+        </v-tabs>
+        
+
+        <v-tabs-items v-model="tab">
+          <v-tab-item key="core_members">
+            <member-list mtype="core_member" @error="error = $event"></member-list>
+          </v-tab-item>
+
+          <v-tab-item key="faculty">
+            <member-list mtype="faculty" @error="error = $event"></member-list>
+          </v-tab-item>
+
+          <v-tab-item key="members">
+            <member-list mtype="member" @error="error = $event"></member-list>
+          </v-tab-item>
+        </v-tabs-items>
+
+      </v-card>
+    </v-flex>
+  </v-layout>
+
+</template>
+
+
+<script>
+import memberList from '@/components/memberList'
+export default {
+  components:{
+    memberList,
+  },
+
+  data:()=>({
+    tab: 'true',
+    
+  })
+  
+}
+</script>
+
+
+
