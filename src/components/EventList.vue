45e652742d6eb2979474242e4e8d9328ece947bc
Anupam Saikia <30382168+anupamsaikia@users.noreply.github.com>
2018-08-22 15:35:01 +0530

    added eventlist view

---
+++ src/components/EventList.vue
@@ -0,0 +1,141 @@
+<template>
+<div>
+  <div v-if="events">
+    <v-list three-line subheader>
+      <template v-for="(event, index) in events">
+        <v-list-tile
+          :key="event.id"
+          avatar
+          @click="()=>{}"
+        >
+          <v-list-tile-avatar color="blue" size="48">
+            <span class="white--text caption">{{ event.time | shortDate }}</span>
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <router-link :to="'/events/'+event.id" class="my-link">
+              <v-list-tile-title class="body-2 grey--text text--darken-3">{{ event.title }}</v-list-tile-title>
+              <v-list-tile-sub-title class="body-1">{{ event.place }}</v-list-tile-sub-title>
+              <v-list-tile-sub-title class="body-1">{{ event.time | niceDateTime}}</v-list-tile-sub-title>
+            </router-link>
+          </v-list-tile-content>
+
+          <v-list-tile-action>
+            <v-list-tile-action-text>{{ event.time | interval }}</v-list-tile-action-text>
+          </v-list-tile-action>
+
+        </v-list-tile>
+        <v-divider
+          v-if="index + 1 < events.length && !event.header"
+          :key="index"
+        ></v-divider>
+      </template>
+    </v-list>
+  </div>
+
+<!-- Delete dialog 
+  <v-dialog v-model="deleteDialog" max-width="600" transition="slide-y-transition">
+    <delete-event :id="deleteEventId" @close="deleteDialog=false"></delete-event>
+  </v-dialog>
+
+ Pagination 
+  <div v-if="events" class="text-xs-center my-5">
+    <v-pagination 
+    :length="length"
+    v-model="page"
+    circle
+    total-visible="7"
+    ></v-pagination>
+  </div>
+-->
+  <div v-if="message" class="my-5">
+    <p class="title py-5 px-2 grey--text text--darken-2" style="text-align:center">{{ message }}</p>
+  </div>
+
+</div>
+</template>
+
+<script>
+import format from 'date-fns/format'
+import distanceInWordsStrict from 'date-fns/distance_in_words_strict'
+
+import firebase from 'firebase/app'
+const db = firebase.firestore()
+const settings = {timestampsInSnapshots: true};
+db.settings(settings);
+
+export default {
+  props: ['tense'],
+  data(){
+    return {
+      //to store all the events
+      events: [],
+      //to show msg in case of no event present
+      message: null,
+    }
+  },
+
+  methods: {
+
+    getData(){
+      this.events = []
+
+      var sign, order
+      if(this.tense == "future"){
+        sign = '>='
+        order = 'asc'
+      }
+      else {
+        sign = '<'
+        order = 'desc'
+      }
+
+      db.collection("events")
+        .where("visibility", "==", "public")
+        .where("eventTime",sign,new Date())
+        .orderBy("eventTime", order)
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            let time = doc.data().eventTime.toDate()
+            this.events.push({...doc.data(), ...{'id' : doc.id, 'time' : time} })
+          });
+          if(this.events.length == 0){
+            this.message = "No event available"
+          }          
+        })
+        .catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
+
+    },
+
+  },
+
+  filters: {
+    niceDateTime: function (value) {
+      if (!value) return ''
+      return format(value,"ddd, D MMM YYYY, h:mm A ")
+    },
+    shortDate: function (value) {
+      if (!value) return ''
+      return format(value,"D MMM")
+    },
+    interval: function (value) {
+      if (!value) return ''
+      return distanceInWordsStrict(
+        new Date(),
+        value,
+        {addSuffix: true}
+      )
+    }
+  },
+
+
+  mounted(){
+    this.getData()
+  }
+}
+
+</script>
+
---
+++ src/views/Events.vue
@@ -0,0 +1,80 @@
+<template>
+<div>
+<!-- Error 
+  <my-error v-if="error" :title="error.title" :description="error.description"></my-error>
+-->
+<!-- Events list -->
+  
+  <v-layout row class="my-3">
+    <v-flex xs12 sm8 offset-sm2>
+      <v-card>
+        <v-tabs
+          v-model="tab"
+          color="grey lighten-3"
+          grow
+        >
+          <v-tabs-slider color="blue"></v-tabs-slider>
+
+          <v-tab :ripple="false" key="upcoming">
+            Upcoming events
+          </v-tab>
+
+          <v-tab :ripple="false" key="past">
+            Past events
+          </v-tab>
+        </v-tabs>
+        
+
+        <v-tabs-items v-model="tab">
+          <v-tab-item key="upcoming">
+            <event-list tense="future" @error="error = $event"></event-list>
+          </v-tab-item>
+
+          <v-tab-item key="past">
+            <event-list tense="past" @error="error = $event"></event-list>
+          </v-tab-item>
+        </v-tabs-items>
+
+      </v-card>
+    </v-flex>
+  </v-layout>
+
+
+<!-- add event dialog 
+  <v-dialog v-model="addEventDialog" max-width="600">
+    <event-editor @close="addEventDialog=false" mode='add'></event-editor>
+  </v-dialog>
+
+ Gap 
+  <div style="height:30px">
+  </div>
+
+
+ Floating Add Button
+  <v-btn
+    color="pink"
+    dark fixed bottom right fab
+    @click="addEventDialog = true"
+    v-if="$store.state.authToken"
+  >
+    <v-icon>add</v-icon>
+  </v-btn>
+-->
+</div>
+</template>
+
+<script>
+import EventList from '@/components/EventList'
+//import EventEditor from '@/components/eventEditor'
+
+export default {
+  data(){
+    return {
+      //error: null,
+      tab: "true",
+      //addEventDialog: false,
+    }
+  },
+  components: { EventList },
+}
+</script>
