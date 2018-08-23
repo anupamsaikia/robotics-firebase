82ddbd85fd08c30a47cbf6fa5bfb369c5137d509
Anupam Saikia <30382168+anupamsaikia@users.noreply.github.com>
2018-08-23 23:32:29 +0530

    added gallery veiw

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,8 +1,11 @@
 <template>
 <div class="my-3">
   <h3 class="px-3">Photo Gallery</h3>
-  <div class="pa-3">
+  <div class="pa-3" v-if="items">
     <vue-picture-swipe :items="items"></vue-picture-swipe>
+  </div>
+  <div v-else>
+    <p class="pa-5">No photo available right now</p>
   </div>
 </div>
 </template>
@@ -10,30 +13,44 @@
 <script>
 import VuePictureSwipe from 'vue-picture-swipe';
 
+import firebase from 'firebase/app'
+const db = firebase.firestore()
+const settings = {timestampsInSnapshots: true};
+db.settings(settings);
+
 export default {
   components:{
     VuePictureSwipe
   },
 
+  mounted(){
+    this.getData()
+
+  },
+
   data(){
     return {
-      items: [{
-          src: 'http://via.placeholder.com/600x400',
-          thumbnail: 'http://via.placeholder.com/100x100',
-          w: 600,
-          h: 400,
-          title: 'Will be used for caption'
-        },
-        {
-          src: 'http://via.placeholder.com/1200x900',
-          thumbnail: 'http://via.placeholder.com/64x64',
-          w: 1200,
-          h: 900
-        }
-      ],
+      items: [],
 
     }
   },
+
+
+  methods:{
+    getData(){
+      db.collection("gallery-photos")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            this.items.push(doc.data())
+          });         
+        })
+        .catch((error) => {
+            console.log("Error getting photos: ", error);
+        });
+
+    }
+  }
   
 
 }
